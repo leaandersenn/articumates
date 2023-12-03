64f68e16eb1c6e13811e893a1d2a9d9660400949
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-12-03 16:33:02 +0100

    Feat: Connected userprofiles to firebase.

--- articumates/frontend/src/app/dashboard/page.tsx
+++ articumates/frontend/src/app/dashboard/page.tsx
@@ -1,23 +1,59 @@
 'use client'
 
-import { FormEvent, useState } from "react";
-import Searchbar from "../components/Searchbar/Searchbar";
-import UserProfile from "../components/UserProfile/UserProfile";
-import './dashboard.css'
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import Searchbar from '../components/Searchbar/Searchbar';
+import UserProfile from '../components/UserProfile/UserProfile';
+import './dashboard.css';
+import { db } from '../../firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+
+interface Impairment {
+  description: string;
+  skillLevel: number;
+}
+
+interface User {
+  goals?: string[];
+  impairments?: Impairment[];
+  name: string;
+  age: number;
+}
 
 
 const Dashboard = () => {
-
+    const [users, setUsers] = useState<User[]>([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    useEffect(() => {
+      const fetchUsers = async () => {
+        const usersRef = collection(db, 'client');
+        const usersSnapshot = await getDocs(usersRef);
+        const usersList = usersSnapshot.docs.map((doc) => doc.data() as User);
+        setUsers(usersList);
+        console.log('Fetched users:', JSON.stringify(usersList, null, 2));
+      };
+      
+      fetchUsers();
+    }, []);
+   
+    const filteredUsers = users.filter(user =>
+        user.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    
+      const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+      };
+    
+  
     return (
-        <div className="flex flex-col items-center">
-             <Searchbar />
-
-            <UserProfile />
-            <UserProfile />
-            <UserProfile />
-         </div>
-       
-    );
+      <div className="flex flex-col items-center">
+        <Searchbar value={searchTerm} onChange={handleInputChange} />
+        {filteredUsers.map((user, index) => (
+            <UserProfile key={index} {...user} />
+        ))}
+      </div>
+  );
 };
 
 export default Dashboard;
+
