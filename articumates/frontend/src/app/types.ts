6fc2643ea81faeec43d3e66894439b6048461d1d
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2023-12-14 09:36:28 +0100

    Feat: Set up redux to have a global state.

---
+++ articumates/frontend/src/app/types.ts
@@ -0,0 +1,10 @@
+export type ChosenImpairment = {
+    description: string;
+    focus: 'High' | 'Medium' | 'Low' | 'No focus';
+  };
+  
+  
+export type Impairment = {
+    description: string;
+    skillLevel: number;
+  };
