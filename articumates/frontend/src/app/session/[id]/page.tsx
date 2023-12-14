53f1218bcf8b139871c10c9b4e3b58c636a62bcc
Eivind Midtbø Øyulvstad <eivindoy@stud.ntnu.no>
2023-12-14 16:55:32 +0100

    changed return to list of strings

--- articumates/frontend/src/app/session/[id]/page.tsx
+++ articumates/frontend/src/app/session/[id]/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import ImpairmentSelector from "@/app/components/ImpairmentSelector/ImpairmentSelector";
 import Impairments from "@/app/components/Impairments/Impairments";
@@ -7,50 +7,48 @@ import WhenBox from "@/app/components/WhenBox/page";
 import { Button, Card, CardBody, CardHeader } from "@nextui-org/react";
 import { useParams, useRouter } from "next/navigation";
 import { Fragment } from "react";
-import './session.css'
-
-
+import "./session.css";
 
 export default function Session() {
+  const params = useParams();
+  const userID = params.id;
+  const router = useRouter();
 
-    const params = useParams();
-    const userID = params.id; 
-    const router = useRouter();
+  const onHandleCancel = () => {
+    router.push(`/users/${userID}`);
+  };
 
-    const onHandleCancel = () => {
-      router.push(`/users/${userID}`);
-    }
+  const onHandleGenerate = () => {
+    /* TODO: Push to the sessionexercises page */
+  };
 
-    const onHandleGenerate = () => {
-      /* TODO: Push to the sessionexercises page */
-    }
+  return (
+    <Card className="sessionCard">
+      <CardHeader className="sessionHeader">Create Session</CardHeader>
+      <CardBody className="sessionBody">
+        <div className="timeProfile">
+          <WhenBox />
+          <Card className="profileCard">
+            <ProfileInfo userID={Number(userID)} />
+          </Card>
+        </div>
+        <p className="exercisesText">Exercises</p>
 
-
-    return(
-        <Card className="sessionCard">
-          <CardHeader className="sessionHeader">
-            Create Session
-          </CardHeader>
-          <CardBody className="sessionBody">
-            <div className="timeProfile">
-                <WhenBox/>
-                <Card className="profileCard">
-                  <ProfileInfo userID={Number(userID)} />
-                </Card>
-            </div>
-            <p className="exercisesText">Exercises</p>
-
-            <div className="textGroup">
-              <p className="impairment">Impairment</p>
-              <p className="skillLevel">SkillLevel</p>
-              <p className="focus">Focus</p>
-            </div>
-            <ImpairmentSelector />
-            <div className="flex flex-row gap-y-4">
-              <Button className="cancelButton" onClick={onHandleCancel}>Cancel</Button>
-              <Button className="generateButton"onClick={onHandleGenerate}>Generate Exercises</Button>
-            </div>
-          </CardBody>
-        </Card>
-    );
+        <div className="textGroup">
+          <p className="impairment">Impairment</p>
+          <p className="skillLevel">SkillLevel</p>
+          <p className="focus">Focus</p>
+        </div>
+        <ImpairmentSelector />
+        <div className="flex flex-row gap-y-4">
+          <Button className="cancelButton" onClick={onHandleCancel}>
+            Cancel
+          </Button>
+          <Button className="generateButton" onClick={onHandleGenerate}>
+            Generate Exercises
+          </Button>
+        </div>
+      </CardBody>
+    </Card>
+  );
 }
