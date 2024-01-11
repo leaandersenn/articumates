063baa2c9367a92846b0cb740a95acfea4bbdde6
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-11 11:12:26 +0100

    Feat: 10 tokes instead on 1000

--- articumates/frontend/src/app/components/LLM/openai.mjs
+++ articumates/frontend/src/app/components/LLM/openai.mjs
@@ -30,7 +30,7 @@ export async function makeRequest(earlierPrompts, prompt) {
       },
     ],
     temperature: 1,
-    max_tokens: 1000,
+    max_tokens: 10,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
