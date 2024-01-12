1b165df60b21660be61d10cf737bda8c97bf961c
leaandersenn <89526348+leaandersenn@users.noreply.github.com>
2024-01-13 00:40:12 +0100

    Feat: readme + button start page

--- articumates/frontend/src/app/page.tsx
+++ articumates/frontend/src/app/page.tsx
@@ -1,11 +1,21 @@
+'use client'
 import React from "react";
 import UserProfile from "./components/UserProfile/UserProfile";
 import { CreateExercises } from "./components/LLM/CreateExercises";
+import { Button } from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const gotoUsers = () => {
+  router.push(`/users/`);
+}
+
   return (
     <div>
-      <h1>Login Page here</h1>;
+      <Button onClick={gotoUsers}>Go to application</Button>
     </div>
   );
 }
